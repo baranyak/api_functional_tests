bb50b81b4598c092dc45162d29059bd2230572a1
Mykhailo Baranyak <mbaranyak@cogniance.com>
2015-06-27 15:13:12 +0300

    First commit

---
+++ core/task_handler.py
@@ -0,0 +1,26 @@
+import requests
+import sys
+import os
+
+if __name__ == '__main__':
+    sys.path.append(os.path.dirname(__file__) + '/../')
+
+import config
+from api_functional_tests.core.task_parser import ResponseParser
+
+
+
+class TaskHandler:
+    @classmethod
+    def get_task(cls, task_id, username, password):
+        raw_response = requests.get(url=config.API_URL+config.TASK_TAIL +
+                                    str(task_id), auth=(username, password))
+        return ResponseParser.get_task(raw_response)
+
+
+
+if __name__ == '__main__':
+    task_1 = TaskHandler.get_task(1, config.DEFAULT_USERNAME,
+                                  config.DEFAULT_PASSWORD)
+    print(task_1)
+
